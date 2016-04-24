ee431f66ad08a8507d4522f93b5a51ccf02f7bf2
Corinna Kuhn <corinnaerin@gmail.com>
2016-04-24 01:09:32 -0700

    Fix placeholder app name in package.json, change to use clean() instead of del()

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,7 +5,7 @@ import babelify from "babelify";
 import browserify from "browserify";
 import buffer from "vinyl-buffer";
 import cleanCSS from "gulp-clean-css";
-import del from "del";
+import clean from "gulp-clean";
 import gulp from "gulp";
 import gutil from "gulp-util";
 import htmlmin from "gulp-htmlmin";
@@ -42,7 +42,8 @@ gulp.task('watch', ['build'], () => {
 });
 
 gulp.task('clean', () => {
-    return del(`${dirs.build}/**/*`)
+    return gulp.src(dirs.build, { read: false })
+        .pipe(clean())
 });
 
 gulp.task('sass', ['clean'], () => {
